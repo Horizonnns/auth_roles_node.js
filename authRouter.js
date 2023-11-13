bcdd018a0851930969b5d7bc1c5c731e11714ae9
Horizonnns <for.office.gthb@gmail.com>
2023-11-13 23:50:19 +0500

    Connected the controller to the router

--- authRouter.js
+++ authRouter.js
@@ -1,8 +1,12 @@
 const Router = require('express');
 const router = new Router();
+const controller = require('./authController');
 
-router.post('/authenticate');
-router.post('/login');
-router.get('/users');
+router.post(
+	'/authenticate',
+	controller.authenticate
+);
+router.post('/login', controller.login);
+router.get('/users', controller.getUsers);
 
 module.exports = router;
