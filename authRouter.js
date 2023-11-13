4d52838d18a970d3279474ec04e61f24fe9b8973
Horizonnns <for.office.gthb@gmail.com>
2023-11-14 00:44:01 +0500

    Made a validation error using a package "express-validator"

--- authRouter.js
+++ authRouter.js
@@ -1,9 +1,21 @@
 const Router = require('express');
 const router = new Router();
 const controller = require('./authController');
+const { check } = require('express-validator');
+const user = require('./models/User');
 
 router.post(
 	'/authenticate',
+	[
+		check(
+			'username',
+			'Имя пользователя не может быть пустым'
+		).notEmpty(),
+		check(
+			'password',
+			'Пароль должен быть больше 4 и меньше 10 символов'
+		).isLength({ min: 4, max: 10 }),
+	],
 	controller.authenticate
 );
 router.post('/login', controller.login);
