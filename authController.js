fdd8a1c2083473b73d4acf73fe8916b28b1976a7
Horizonnns <for.office.gthb@gmail.com>
2023-11-14 00:34:51 +0500

    Created a basic registration with password hashing

--- authController.js
+++ authController.js
@@ -1,9 +1,42 @@
+const User = require('./models/User');
+const Role = require('./models/Role');
+const bcrypt = require('bcryptjs');
+
 class authController {
 	async authenticate(req, res) {
 		try {
-			res.json('auth working');
+			const { username, password } = req.body;
+			const candidate = await User.findOne({
+				username,
+			});
+			if (candidate) {
+				return res.status(400).json({
+					message:
+						'Пользователь с таким именем уже существует',
+				});
+			}
+			const hashPassword = bcrypt.hashSync(
+				password,
+				7
+			);
+			const userRole = await Role.findOne({
+				value: 'USER',
+			});
+			const user = new User({
+				username,
+				password: hashPassword,
+				roles: [userRole.value],
+			});
+			await user.save();
+			return res.json({
+				message:
+					'Пользователь успешно зарегистрирован',
+			});
 		} catch (e) {
 			console.log(e, 'error');
+			res.status(400).json({
+				message: 'Registration error',
+			});
 		}
 	}
 
@@ -12,6 +45,9 @@ class authController {
 			res.json('login working');
 		} catch (e) {
 			console.log(e, 'error');
+			res.status(400).json({
+				message: 'Login error',
+			});
 		}
 	}
 
