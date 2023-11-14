6c4317bd82f9163a5a5dfbc4ba839dee8663ac8b
Horizonnns <for.office.gthb@gmail.com>
2023-11-14 22:50:45 +0500

    Created a user registration with validation, used jwt to generate a token

--- authController.js
+++ authController.js
@@ -4,6 +4,19 @@ const bcrypt = require('bcryptjs');
 const {
 	validationResult,
 } = require('express-validator');
+const jwt = require('jsonwebtoken');
+const { secret } = require('./config');
+
+const generateAccessToken = (id, roles) => {
+	const payload = {
+		id,
+		roles,
+	};
+
+	return jwt.sign(payload, secret, {
+		expiresIn: '24h',
+	});
+};
 
 class authController {
 	async authenticate(req, res) {
@@ -52,7 +65,35 @@ class authController {
 
 	async login(req, res) {
 		try {
-			res.json('login working');
+			const { username, password } = req.body;
+
+			const user = await User.findOne({
+				username,
+			});
+
+			if (!user) {
+				return req.status(400).json({
+					message: `Пользователь ${username} не найден`,
+				});
+			}
+
+			const validPassword = bcrypt.compareSync(
+				password,
+				user.password
+			);
+
+			if (!validPassword) {
+				return res.status(400).json({
+					message: 'Введен неверный пароль',
+				});
+			}
+
+			const token = generateAccessToken(
+				user._id,
+				user.roles
+			);
+
+			return res.json({ token });
 		} catch (e) {
 			console.log(e, 'error');
 			res.status(400).json({
